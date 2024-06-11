e3daa7b6e6a1279899772183f9e2560737fc806b
bhagyashree <bhagyashree@gmail.com>
2024-06-11 16:02:19 +0530

    Update the app

---
+++ Calculate Square & Cube.py
@@ -0,0 +1,36 @@
+# # Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+def square(num):
+    return num*num
+num = 2
+print(format(f'The Square of {num} is: {square(num)}'))
+
+
+def cube(num1):
+    return num1*num1*num1
+num1=2
+print(format(f'The Cube of {num1} is: {cube(num1)}'))
+
+# OR
+
+# # Python Program to Find & Cube Cube of a Number
+# num = int(input("Enter an integer: "))
+# # Calculating cube
+# square= num * num
+# cube = num * num * num
+# # Displaying output
+# print("Square of {0} is {1}".format(num,square))
+# print("Cube of {0} is {1}" .format(num, cube))
+
+
+# OR
+
+# num = 2
+# # Calculating cube
+# square= num * num
+# cube = num * num * num
+# # Displaying output
+# print("Square of {0} is {1}".format(num,square))
+# print("Cube of {0} is {1}" .format(num, cube))
+
+
+
---
+++ Check Number Graeter Than Or Not.py
@@ -0,0 +1,24 @@
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+First_Number= (input("Enter First Number:"))
+Second_Number= (input("Enter Second Number:"))
+if First_Number>Second_Number:
+    print("First Number is Greater")
+elif First_Number<Second_Number:
+    print("Second Number is Greater")
+else:
+    print("First Number  equal to Second Number" )
+
+
+#OR
+
+# Number1 = int(input('please enter your first number'))
+#
+# Number2 = int(input('please enter your second number'))
+#
+# if Number1 > Number2:
+#     print('Number1 bigger than Number2')
+# elif Number1 < Number2:
+#     print('Number1 smaller than Number2')
+# else:
+#     print('Number1 equal to Number2')
