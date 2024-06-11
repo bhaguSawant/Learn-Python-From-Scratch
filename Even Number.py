f9b5e3ccefed36af5e1951e701ed35887d5a0a12
bhagyashree <bhagyashree@gmail.com>
2024-06-11 16:06:37 +0530

    Even Number Program

---
+++ Even Number.py
@@ -0,0 +1,28 @@
+#for i in range(2, 51, 2):
+#   print(i)
+
+
+#Addition of list number#
+#Number=[2,4,6,7]
+#total_sum= sum(Number)
+#print("Sum of all number :", total_sum)
+
+
+#create a list of strings and print the length of each string#
+
+# Create a list of strings
+#strings = ["hello", "world", "Python", "programming", "is", "fun"]
+
+# Calculate and print the length of each string
+#for s in strings:
+ #  print(f"The length of '{s}' is {len(s)} characters.")
+
+#Create a List of number and Sort them
+#Number=[5,7,10,1,2,70]
+#sorted_Numbers=sorted(Number)
+#print((sorted_Numbers))
+
+#Create Alist of number find maxamize the list number
+number=[40,100,30,55,120]
+max_number=max(number)
+print(max_number)
